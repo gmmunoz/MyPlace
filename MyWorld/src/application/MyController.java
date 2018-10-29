8e606add4eb8fb7140af76dc682357ba068e4cc9
Gabi Munoz <gmm2172@columbia.edu>
2018-10-29 14:57:34 -0400

    Now Gabi's files are integrated into github repo

--- MyWorld/src/application/MyController.java
+++ MyWorld/src/application/MyController.java
@@ -1,8 +1,7 @@
 package application;
 
-import java.io.IOException;
 import java.net.URL;
-//import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,11 +17,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-/*<<<<<<< HEAD
-//import com.gluonhq.charm.glisten.control.TextField;
-=======
->>>>>>> 502959*/
 
 public class MyController implements Initializable {
 	
@@ -31,7 +27,12 @@ public class MyController implements Initializable {
 		// TODO Auto-generated method stub
 		
 	}
-	    @FXML
+	 
+	@FXML
+	private AnchorPane backgroundRoot;
+	
+
+	@FXML
 	    private TextField txtUserName;
 
 	    @FXML
@@ -46,31 +47,92 @@ public class MyController implements Initializable {
 	    @FXML
 	    private Button signupBut;
 
-	   // private Connection con = null;
+	    private Connection con = null;
 	    private PreparedStatement ps = null;
 		private ResultSet rs = null;
 
 		public MyController() {
-			//con = ConnectionDB.connect();
+			con = ConnectionDB.connect();
 		}
 		
 		 //action to register
 	    @FXML
 	    void signUp(ActionEvent event) throws Exception{
 	    	try {
-	    		System.out.println("You clicked Sign Up button");
+	    		System.out.println("You clicked Sign Up button!");
+	    		
+	    		//now load Register page
+	    		AnchorPane pane = FXMLLoader.load(getClass().getResource("../fxml/RegisterFXML.fxml"));
+	    		backgroundRoot.getChildren().setAll(pane);
+		    	
+	    	} catch (Exception ex) {
+	    		ex.printStackTrace();
+	    	}    	
+	  
+	    }  
+
+	    //attempt to login
+	    @FXML
+	    void logIn(ActionEvent event) {
+	    	// decipher inputed username & password
+	    				String userName = txtUserName.getText().trim();
+	    				String passwd = Password.getText().trim();
+
+	    				// SQL connect now
+	    				String sql = "SELECT * user WHERE userName = ? AND passwd = ?";
+	    				try {
+	    					ps = con.prepareStatement(sql);
+	    					ps.setString(1, userName);
+	    					ps.setString(2, passwd);
+	    					rs = ps.executeQuery();
+
+	    					if (rs.next()) {
+	    						// if username + password exist & match --> main page
+	    						System.out.println("You logged in successfully!");    			    		 
+	    						  
+	    			    		AnchorPane pane = FXMLLoader.load(getClass().getResource("../fxml/MainPageFXML.fxml"));
+	    			    		backgroundRoot.getChildren().setAll(pane);
+	    			    			
+	    						
+	    					} else {
+	    						// show error --> try again
+	    						System.out.println("You entered wrong password or username!");
+	    			    		   
+	    			    		AnchorPane pane = FXMLLoader.load(getClass().getResource("../fxml/WrongTryAgainFXML.fxml"));
+	    			    		backgroundRoot.getChildren().setAll(pane);
+
+//	    						Parent root = FXMLLoader.load(getClass().getResource("../fxml/WrongTryAgainFXML.fxml"));
+//
+//	    						Scene scene = new Scene(root);	    
+//	    				    	Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();	    
+//	    				    	stage.setTitle("Error: Try Again");
+//	    				    	stage.setScene(scene);	  
+//	    				    	stage.show();	    				    	
+	    						
+	    					}	 
+
+	    				} catch (Exception ex) {
+	    					ex.printStackTrace();
+	    				}
+	    }
+	    
+	    
+
+	    @FXML
+	    void retrievePwd(ActionEvent event) {
+	    	try {
+	    			
+	    		System.out.println("You clicked Forgot Password button!");
 	    		   
-		    	Parent root = FXMLLoader.load(getClass().getResource("/RegisterFXML.fxml"));	    
+		    	Parent root = FXMLLoader.load(getClass().getResource("../fxml/forgotPwdFXML.fxml"));	    
 		    	Scene scene = new Scene(root);	    
 		    	Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();	    
-		    	stage.setTitle("Register");
+		    	stage.setTitle("Forgot Password");
 		    	stage.setScene(scene);	  
 		    	stage.show();
 		    	
 	    	} catch (Exception ex) {
 	    		ex.printStackTrace();
 	    	}    	
-	  
 	    }
-}    
-
+}
--- MyWorld/src/application/RegisterController.java
+++ MyWorld/src/application/RegisterController.java
@@ -1,30 +1,73 @@
 package application;
 
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 
 public class RegisterController {
 	
+	ObservableList<String> questionList1 = FXCollections.observableArrayList("What is the name of your first pet?", "What is your mother's maiden name?","What is your dream job?");
+		
+	ObservableList<String> questionList2 = FXCollections.observableArrayList("What is your favorite color?", "What is your father's middle name?","What was your childhood nickname?");
+	
+	@FXML
+    private AnchorPane backgroundRoot;
+	
 	@FXML
 	private TextField txtUserName;
-
-	@FXML
-	private TextField email;
 	  
 	@FXML	
 	private Button regBut;
 	
 	@FXML	
 	private PasswordField Password;
+		 
+	@FXML	
+	private ComboBox<String> Question1;
 	
+	@FXML	
+	private ComboBox<String> Question2;
+	
+	@FXML
+    private TextField q2answer;
+
+    @FXML
+    private TextField q1answer;
+	
+	
+	 @FXML
+	    void initialize() {
+		 Question1.setValue("Security Question #1");
+		 Question1.setItems(questionList1);
+		 
+		 Question2.setValue("Security Question #2");
+		 Question2.setItems(questionList2);
+	 }
 	@FXML	
 	void handleRegBut(ActionEvent event) {
 
-		   
+		//NEED to save info into database (username/pwd/questions 1&2)
+		//here is where you add info & save it to DB
+		
+		//return to login page
+		System.out.println("You are now registered! Now try to login! ");    			    		 		  		
+		try {
+			AnchorPane pane = FXMLLoader.load(getClass().getResource("../fxml/MyPlace_login.fxml"));
+			backgroundRoot.getChildren().setAll(pane);
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+				   
 	}	
 }
