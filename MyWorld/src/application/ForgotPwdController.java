7a9fd32e6eaf60d2c83df8a75986d1d8f82ed7a3
Gabi Munoz <gmm2172@columbia.edu>
2018-10-29 00:02:56 -0400

    Now actually have pushed files

---
+++ MyWorld/src/application/ForgotPwdController.java
@@ -0,0 +1,23 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class ForgotPwdController {
+	
+    @FXML
+    private Button forgotBut;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    void handleForgotBut(ActionEvent event) {
+
+    }
+
+}
