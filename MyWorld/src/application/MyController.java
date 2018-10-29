84c0d297abf85717604584a00865ece5327a7553
Gabi Munoz <gmm2172@columbia.edu>
2018-10-29 00:02:42 -0400

    Have now created controller classes for login page

--- MyWorld/src/application/MyController.java
+++ MyWorld/src/application/MyController.java
@@ -2,22 +2,23 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
+//import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import com.gluonhq.charm.glisten.control.TextField; 
 
 public class MyController implements Initializable {
 	
@@ -41,78 +42,31 @@ public class MyController implements Initializable {
 	    @FXML
 	    private Button signupBut;
 
-	    private Connection con = null;
+	   // private Connection con = null;
 	    private PreparedStatement ps = null;
 		private ResultSet rs = null;
 
 		public MyController() {
-			con = ConnectionDB.connect();
+			//con = ConnectionDB.connect();
 		}
 		
 		 //action to register
 	    @FXML
-	    void signUp(ActionEvent event) throws IOException{
-	    	Parent root = FXMLLoader.load(getClass().getResource("RegisterFXML.fxml"));
-
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root));
-			stage.setTitle("Register");
-			stage.show();
+	    void signUp(ActionEvent event) throws Exception{
+	    	try {
+	    		System.out.println("You clicked Sign Up button");
+	    		   
+		    	Parent root = FXMLLoader.load(getClass().getResource("/RegisterFXML.fxml"));	    
+		    	Scene scene = new Scene(root);	    
+		    	Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();	    
+		    	stage.setTitle("Register");
+		    	stage.setScene(scene);	  
+		    	stage.show();
+		    	
+	    	} catch (Exception ex) {
+	    		ex.printStackTrace();
+	    	}    	
+	  
 	    }
-	    
-	    //action to attempt to login
-	    //@MICHAELLA LOOK HERE FOR SQL
-	    @FXML
-	    void logIn(ActionEvent event) {
-	    	// decipher inputed username & password
-			String userName = txtUserName.getText().trim();
-			String passwd = Password.getText().trim();
+}    
 
-			// SQL connect now
-			String sql = "SELECT * user WHERE userName = ? AND passwd = ?";
-			try {
-				ps = con.prepareStatement(sql);
-				ps.setString(1, userName);
-				ps.setString(2, passwd);
-				rs = ps.executeQuery();
-
-				if (rs.next()) {
-					// this where new page fxml gets loaded
-					Parent root = FXMLLoader.load(getClass().getResource("MainPageFXML.fxml"));
-
-					Stage stage = new Stage();
-					stage.setScene(new Scene(root));
-					stage.setTitle("Main Page");
-					stage.show();
-
-				} else {
-					// show error --> try again
-					// NEED TO CREATE WRONG PASSWORD FXML
-					Parent root = FXMLLoader.load(getClass().getResource("WrongTryAgainFXML.fxml"));
-
-					Stage wrongStage = new Stage();
-					wrongStage.setScene(new Scene(root));
-					wrongStage.setTitle("Wrong Username or Password");
-					wrongStage.centerOnScreen();
-					wrongStage.show();
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-	   
-
-	    //Action to retrieve forgotten password
-	    @FXML
-	    void retrievePwd(ActionEvent event) throws IOException {
-	    	// load forgot password page
-			Parent root = FXMLLoader.load(getClass().getResource("ForgotPasswordFXML.fxml"));
-
-			Stage wrongStage = new Stage();
-			wrongStage.setScene(new Scene(root));
-			wrongStage.setTitle("Retrieve Password");
-			wrongStage.centerOnScreen();
-			wrongStage.show();
-		}
-
-}
