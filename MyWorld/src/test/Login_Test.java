30384c68bd0e5dcb2f4772f4545cd894ebc6a06f
francesca-callejas <fcallejas97@gmail.com>
2018-10-30 12:37:48 -0400

    Test Cases - database functions
    CreateAccount_Test.java
    Login_Test
    --functioning test classes
    
    still working on JavaFX part

--- MyWorld/src/test/Login_Test.java
+++ MyWorld/src/test/Login_Test.java
@@ -1,9 +1,50 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import application.DataConnection;
+
 public class Login_Test {
 
-	public Login_Test() {
-		// TODO Auto-generated constructor stub
+	DataConnection dataConnection = new DataConnection(); 
+	
+	/* Test valid login credentials*/
+	@Test
+	void login_valid() throws Exception {
+		String usernameTest = "test"; 
+		String passwordTest = "test123"; 
+		String securityqTest = "12"; 
+		
+		dataConnection.addUser(usernameTest, passwordTest, securityqTest, securityqTest); 
+		
+		assertTrue(dataConnection.userExists(usernameTest));
+		dataConnection.deleteUser(usernameTest); 
+		
 	}
+	
+	/* Test invalid login credentials -- empty strings*/
+	@Test
+	void login_emptyField() throws Exception {
+		String usernameAttempt = ""; 
+		String passwordTest = ""; 
+		String securityqTest = "12"; 
+		
+		assertFalse(dataConnection.validAccount(usernameAttempt, passwordTest)); 
+		
+	}
+	
+	/* Test invalid login credentials -- wrong password*/
+	void login_wrongPassword() throws Exception{
+		String usernameTest = "test"; 
+		String passwordTest = "test123"; 
+		String securityqTest = "12"; 
+		
+		dataConnection.addUser(usernameTest, passwordTest, securityqTest, securityqTest); 
+		
+		String incorrectPassword = "Test123"; 
+		assertFalse(dataConnection.validAccount(usernameTest, incorrectPassword));
+		dataConnection.deleteUser(usernameTest);
+	}
+	
 
 }
