1b883a65437f2c8d62c2475c6649bbae5b8cbf36
francesca-callejas <fcallejas97@gmail.com>
2018-10-30 00:56:28 -0400

    Latest updates on tests

--- MyWorld/src/application/ConnectionDB.java
+++ MyWorld/src/application/ConnectionDB.java
@@ -5,14 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-<<<<<<< HEAD
-	/*Connection conn = null;
-    public static Connection connect()
-=======
 	Connection conn = null;
 
 	public static Connection connect()
->>>>>>> ecbc26eb4f33629da25b01e72991ab0e337ccc24
     {
         try {
         	//@MICHAELLA
