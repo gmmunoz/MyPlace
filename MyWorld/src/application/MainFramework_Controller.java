7950227a12c3e25d420fb3017cbf5927ca8bb88d
Gabi Munoz <gmm2172@columbia.edu>
2018-11-21 22:07:51 -0800

    now login transitions to main page

---
+++ MyWorld/src/application/MainFramework_Controller.java
@@ -0,0 +1,36 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFramework_Controller {
+
+    @FXML
+    private AnchorPane backgroundRoot;
+
+    @FXML
+    private Button PlacesYGbut;
+
+    @FXML
+    private Button PlacesYBbut;
+
+    @FXML
+    private Button LogoutBut;
+
+    @FXML
+    void DirectPlacesYB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DirectPlacesYG(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LogoutUser(ActionEvent event) {
+
+    }
+
+}
